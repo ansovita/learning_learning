4fb9a454244751bfbb6ba67bc49da55adbfbd8bc
ansovita <tatjana.ansovica@gmail.com>
2024-05-29 20:45:01 +0300

    [classroom/290524]: Add Variables.java

---
+++ src/main/java/lv/acodemy/classroom/Variables.java
@@ -0,0 +1,23 @@
+package lv.acodemy.classroom;
+
+public class Variables {
+    public static void main(String[] args) {
+        //        Category: Integral
+
+        //      byte:
+        byte smallNumber = 120;
+        // RGB (Red Green Blue)
+        byte red = 120;
+        byte green = 45;
+        byte blue = 127;
+        System.out.println(blue);
+
+        // int (Integer)
+        // Employee ID
+        int employeeId = 12345;
+
+        //Population count
+        int populationOfLatvia = 1811956;
+        System.out.println(populationOfLatvia);
+    }
+}
